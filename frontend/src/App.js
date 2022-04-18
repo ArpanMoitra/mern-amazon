783c412926c83ccc71c886b0d0b4e0122c0ebed7
ArpanMoitra <arpan.it19@nsut.ac.in>
2022-04-18 21:51:55 +0530

    Create-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">Amazon</a>
+      </header>
+      <main>List Items.</main>
+    </div>
+  );
+}
+
+export default App;
